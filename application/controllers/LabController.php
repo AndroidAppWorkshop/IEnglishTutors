1a8a1c85820d1fb5614c55f77cb435f91ef1cd52
LuHua10 <lovero32000@gmail.com>
2015-08-04 22:56:32 +0800

    Add the LabController(fot the email)

---
+++ application/controllers/LabController.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class LabController extends CI_Controller {
+
+	public function Index()
+	{
+		$this->load->view('view_email');
+
+		$data_post_name = $this->input->post('YourName', TRUE);
+		$data_post_mail = $this->input->post('YourMail', TRUE);
+		$data_post_subject = $this->input->post('YourSubject', TRUE);		
+		$data_post_message = $this->input->post('YourMessage', TRUE);
+
+		
+
+	}
+
+	public function clickSend(){
+
+		$config = Array(
+			'protocol' => 'smtp',
+			'smtp_host' => 'ssl://smtp.gmail.com',
+			'smtp_port' => '465',
+			'smtp_user' => '*****@gmail.com',//當作客服的信箱
+			'smtp_pass' => '*****'
+		);
+
+		$this->load->library('email' , $config );
+		$this->email->set_newline("\r\n");	
+
+		$this->email->from('lovero32000@gmail.com', 'Hua Lu');//寄件者姓名
+		$this->email->to('joelaio@yahoo.com.tw');			  //要寄給誰
+		$this->email->subject('this is an email subject');    //信件標題
+		$this->email->message('this is the mail content');    //信件內容
+
+		if ($this->email->send()) {
+			echo 'your email was sent';
+		}
+		else{
+			show_error($this->email->print_debugger());
+		}
+	}
+}
