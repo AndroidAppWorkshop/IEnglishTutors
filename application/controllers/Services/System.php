04bd3c447b883f06691c729326efbd0a572fa50d
Wilson <l7960261@gmail.com>
2015-08-06 02:08:49 +0800

    Add LanguageWithResult & LanguageWithMaterialization function

--- application/controllers/Services/System.php
+++ application/controllers/Services/System.php
@@ -1,4 +1,5 @@
 <?php
+require_once '/application/models/dbo/Language.php';
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class System extends CI_Controller {
@@ -14,15 +15,32 @@ class System extends CI_Controller {
 		exit;
 	}
 
-	public function Language() {
+	public function LanguageWithResult() {
 		$this->load->database();
 		$query = $this->db->get('language');
 
 		foreach ($query->result() as $row)
 		{
 		        echo $row->Id;
+		        echo '|';
+		        echo $row->Name;
+		        echo '<br>';
 		}
 
 		exit;
+	}
+
+	public function LanguageWithMaterialization() {
+		$this->load->database();
+		$query = $this->db->get('language');
+
+		foreach ($query->result('Language') as $language)
+		{
+        	echo $language->Id;
+        	echo '|';
+        	echo $language->Name;
+        	echo '<br>';
+        	//echo $language->reverse_name(); // or methods defined on the 'User' class
+		}
 	}
 }
