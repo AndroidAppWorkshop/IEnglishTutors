68ec5f69453787a5988b91da3558d810990ad2bc
Wilson <l7960261@gmail.com>
2015-08-13 22:26:39 +0800

    Modify IET_Loader & Add Router Model to process current route

--- application/core/IET_Loader.php
+++ application/core/IET_Loader.php
@@ -10,45 +10,25 @@ class IET_Loader extends CI_Loader {
         parent::__construct();
         
         $this->CI = get_instance();
+        $this->model('Shared/Router');
     }
 
     public function Render($view = '', $vars = array(), $return = FALSE)
     {
-        $this->SetCurrentPath();
-        $viewPath = $this->GetViewPath($view);
+        $path = $this->CI->Router->GetCurrentPath($view);
 
     	$this->view('Shared/Header', $this->GetHeaderData());
-    	$this->view($viewPath, $vars, $return);
+    	$this->view($path, $vars, $return);
     	$this->view('Shared/Footer');
-    }
-
-    protected function SetCurrentPath()
-    {
-        $this->currentController = $this->CI->uri->segment(1, $this->CI->router->default_controller);
-        $this->currentFunction = $this->currentController === $this->CI->router->default_controller ? 'Lobby' : $this->CI->uri->segment(2, 'Index');
-    }
-
-    protected function GetViewPath($view)
-    {
-        if(strcmp($view, '') === 0)
-        {
-            return $this->currentController.'/'.$this->currentFunction;
-        }
-
-        return $view;
     }
 
     protected function GetHeaderData()
     {
-        $data['ViewJson'] = $this->GetViewJson();
-        return $data;
-    }
+        $path = $this->CI->Router->GetCurrentPathWithColon();
+        $viewJson = $this->CI->Layout->ViewJson($path);
 
-    protected function GetViewJson()
-    {
-        $path = $this->currentController.':'.$this->currentFunction;
-        
-        return $this->CI->Layout->ViewJson($path);
+        $data['ViewJson'] = $viewJson;
+        return $data;
     }
 }
 
