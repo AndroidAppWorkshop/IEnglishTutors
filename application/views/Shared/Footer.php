777cd4ce2893e777aa9701dd91504f2f219bd915
Wilson <l7960261@gmail.com>
2015-08-14 21:52:52 +0800

    modify Footer.php & ignore /node_modules/*

--- application/views/Shared/Footer.php
+++ application/views/Shared/Footer.php
@@ -1,4 +1,4 @@
-	<script src="/assets/lib/jquery/js/jquery.min.js"></script>
+	<script src="/assets/lib/jquery/jquery.min.js"></script>
 	<script src="/assets/lib/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
