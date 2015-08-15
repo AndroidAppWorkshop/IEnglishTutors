a62ad7e20cc527a64f82f706bc1602343df095c3
LuHua10 <lovero32000@gmail.com>
2015-08-15 12:14:47 +0800

    add the index page for Lab Controller

--- application/controllers/Lab.php
+++ application/controllers/Lab.php
@@ -2,6 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Lab extends CI_Controller {
+	
+	public function Index()
+	{
+		$this->load->Render();
+	}
 
 	public function SendEmail()
 	{
