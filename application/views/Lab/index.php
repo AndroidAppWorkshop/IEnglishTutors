0a137fb2dc7b5df6a1b0fe8e07b92273b364e43a
Wilson <l7960261@gmail.com>
2015-08-17 00:26:02 +0800

    Modify Lab/Index.php

--- application/views/Lab/index.php
+++ application/views/Lab/index.php
@@ -28,6 +28,86 @@
 				<td>發送 Email</td>
 				<td>LuHua10</td>
 			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/LoadView" role="button">Link</a>
+				</th>
+				<td>LoadView</td>
+				<td>返回讀取那個頁面的整個 HTML</td>
+				<td>Wilson</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/LoadModel" role="button">Link</a>
+				</th>
+				<td>LoadModel</td>
+				<td>返回 Layout -> ViewJson</td>
+				<td>Wilson</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/SetSessionData" role="button">Link</a>
+				</th>
+				<td>SetSessionData</td>
+				<td>設置 Session</td>
+				<td>Wilson</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/GetSessionData" role="button">Link</a>
+				</th>
+				<td>GetSessionData</td>
+				<td>取得 Session</td>
+				<td>Wilson</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/UnsetSessionData" role="button">Link</a>
+				</th>
+				<td>UnsetSessionData</td>
+				<td>移除 Session (部分)</td>
+				<td>Wilson</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/DestroySession" role="button">Link</a>
+				</th>
+				<td>DestroySession</td>
+				<td>銷毀 Session (當前所有)</td>
+				<td>Wilson</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/Languages" role="button">Link</a>
+				</th>
+				<td>Languages</td>
+				<td>獲取當前瀏覽器資訊</td>
+				<td>Wilson</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/SetCookie" role="button">Link</a>
+				</th>
+				<td>SetCookie</td>
+				<td>設置 Cookie</td>
+				<td>Wilson</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/GetCookie" role="button">Link</a>
+				</th>
+				<td>GetCookie</td>
+				<td>獲取 Cookie</td>
+				<td>Wilson</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<a class="btn btn-primary" href="Lab/DeleteCookie" role="button">Link</a>
+				</th>
+				<td>DeleteCookie</td>
+				<td>刪除 Cookie</td>
+				<td>Wilson</td>
+			</tr>
 		</tbody>
 	</table>
 </div>
