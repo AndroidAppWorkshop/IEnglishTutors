df34bbef419529c69a11d1206a36c358cf48f890
LuHua10 <lovero32000@gmail.com>
2015-08-17 21:42:00 +0800

    add the Upload Teaching Material page

--- application/views/Lab/Upload_Form.php
+++ application/views/Lab/Upload_Form.php
@@ -1,71 +1,44 @@
-<div class="container" role="tabpanel" style="width:666px">
-
-  <!-- Nav tabs -->
-  <ul class="nav  nav-tabs" role="tablist">
-    <li role="presentation" class="active">
-      <a 
-        href="#uploadByURL" 
-        aria-controls="uploadByURL" 
-        role="tab" 
-        data-toggle="tab">貼上圖片網址
-      </a>
-    </li>
-
-    <li role="presentation">
-      <a 
-        href="#uploadByFILE" 
-        aria-controls="uploadByFILE" 
-        role="tab" 
-        data-toggle="tab">上傳圖片
-      </a>
-    </li>
-  </ul>
-
-  <!-- Tab panes -->
-  <div class="tab-content">
-    <div role="tabpanel" class="tab-pane active" id="uploadByURL">
-      <form action="DoDownload" method="post" enctype="multipart/form-data">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="input-group">
-              <input 
-                type="text" 
-                name="TargetURL" 
-                class="form-control" 
-                placeholder=""
-                required/>
-              <span class="input-group-btn">
-                <input 
-                  class="btn btn-default" 
-                  type="submit"
-                  value="下載"
-                />
-              </span>
-            </div><!-- /input-group -->
-          </div><!-- /.col-lg-12 -->
-        </div><!-- /.row -->
-      </form>
-    </div>
-
-    <div role="tabpanel" class="tab-pane" id="uploadByFILE">
-      <form action="DoUpload" method="post" enctype="multipart/form-data">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="input-group">
-                        
-              <input type="file" name="PhotoFile">
-                <span class="input-group-btn">
-                  <input 
-                    class="btn btn-default" 
-                    type="submit"
-                    value="上傳"
-                  />
-                </span>
-            </div><!-- /input-group -->
-          </div><!-- /.col-lg-12 -->
-        </div><!-- /.row -->
-      </form>
-    </div>
-  </div>
-
+<div class="container">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<h4 class="modal-title">Upload Teaching Material</h4>
+			</div>
+			<div class="modal-body">
+				<form action="DoUpload" method="post" enctype="multipart/form-data">
+					<table class="table">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>教材名稱</th>
+								<th>上傳路徑</th>
+							</tr>
+						</thead>
+						<tbody class="table-striped">
+							<tr>
+								<th>1</th>
+								<td><input type="text" class="form-control" id="" placeholder="請輸入教材名稱"></td>
+								<td><input type="file" name="File1"></td>
+							</tr>
+							<tr>
+								<th>2</th>
+								<td><input type="text" class="form-control" id="" placeholder="請輸入教材名稱"></td>
+								<td><input type="file" name="File2"></td>
+							</tr>
+							<tr>
+								<th>3</th>
+								<td><input type="text" class="form-control" id="" placeholder="請輸入教材名稱"></td>
+								<td><input type="file" name="File3"></td>
+							</tr>
+						</tbody>
+					</table>
+					<div class="modal-footer">
+						<button type="reset" class="btn btn-default" data-dismiss="modal">Clear</button>
+						<button type="submit" class="btn btn-primary">Upload</button>
+					</div>
+				</form>
+			</div><!-- /.modal-body -->
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
 </div>
